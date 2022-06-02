697c3d2f07b1cb8060c75c21f1bad3defcd3361d
anaeitan14 <77848369+anaeitan14@users.noreply.github.com>
2022-06-02 07:30:47 +0300

    Update SongTest.java

--- q5spring2022/SongTest.java
+++ q5spring2022/SongTest.java
@@ -4,18 +4,18 @@ public class SongTest {
 
 	public static void main(String[] args) {
 		Song[] songList = new Song[3];
-		songList[0] = new Song("King Crimson", 5, "Schizoid man");
-		songList[1] = new Song("THC", 4, "Sweet caroline");
-		songList[2] = new Song("Marianoa", 8, "Okay");
-		Disc myDisk = new Disc("Crimson king", songList);
+		songList[0] = new Song("okok", 5, "song1");
+		songList[1] = new Song("two", 4, "song1");
+		songList[2] = new Song("twotwo", 8, "song1");
+		Disc myDisk = new Disc("song12", songList);
 
 		Song[] songList2 = new Song[5];
-		songList2[0] = new Song("King Crimson", 5, "Schizoid man");
-		songList2[1] = new Song("THC", 4, "Sweet caroline");
-		songList2[2] = new Song("Lmao", 3, "Okay");
-		songList2[3] = new Song("okay Dude", 8, "Okay");
-		songList2[4] = new Song("Marianoa", 8, "Okay");
-		Disc myDisk2 = new Disc("Comfortably numb", songList2);
+		songList2[0] = new Song("songtwo", 5, "sirsir");
+		songList2[1] = new Song("ok", 4, "to");
+		songList2[2] = new Song("no", 3, "yes");
+		songList2[3] = new Song("song3", 8, "song5");
+		songList2[4] = new Song("song9", 8, "song6");
+		Disc myDisk2 = new Disc("song10", songList2);
 
 		Song ns = new Song("Ququ", 120, "Buku");
 		System.out.println(ns.status());
